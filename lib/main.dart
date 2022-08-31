7e65c36370b79c173c4ea0449067babb5e22ec3a
Pranshu Agrawal <pranshukumar1999@gmail.com>
2022-08-31 10:53:06 +0530

    Ch2: Conatiners

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   return runApp(MaterialApp(
+    // MaterialApp() is basically a constructor that we use make widgets in flutter.
     home: Scaffold(
       // It provides a blank screen that is hsown in the display
       appBar: AppBar(
         title: Text("Hello, World!!!"),
       ),
+      body: Container(
+        decoration: BoxDecoration(
+          // BoxDecoration is the most used
+          color: Colors.orange, // predefined constant for color
+        ),
+        child: Text("One"), // Widget Inside the COntainer
+      ), // Container is the div is Flutter, we can use it to constraint
     ),
   ));
 }
